9e802e4b75e3934db9bb3d136f0f140e334ae66d
Skiislam <Sisla016@ucr.edu>
2022-10-31 11:10:31 -0700

    Instead of crowding up Main with different prints to get user input, instead made functions that grab user input

--- CS170P1.py
+++ CS170P1.py
@@ -1,17 +1,46 @@
-import queue
 import sys
+import time
 
+goalPuzzle = (['1', '2', '3', ], ['4', '5', '6'], ['7', '8', '0'])
 
 def main():
     print('Welcome to the 8 Puzzle Solver \n')
-    print('Would you like to use our puzzle, or create your own? \n')
-    userInput = input('Press 1 for the default and 2 if you want to input your own!')
-    choice = int(userInput)
+    UserPuzzle = userChoice()
+    strat = userStrategy()
 
-    if choice == 1:
-        puzzle = (['1', '3', '5', ], ['4', '6', '8'], ['2', '7', '8'])
-    elif choice == 2:
-        print('Please begin entering your puzzle and use 0 to represent a blank spot \n')
-        
+def userChoice():
+    print('Would you like to use our puzzle or create youf own?\n')
+    userInput = input('Press 1 to see ours, or 2 if you want to create your own')
+    uInput = int(userInput)
+    if uInput == 1:
+       puzzle = (['1', '3', '5', ], ['4', '6', '8'], ['2', '7', '8'])
+       return puzzle
+    elif uInput == 2:
+        return userPuzzle()
+    else:
+        print("Error could not recongize input, please enter 1 or 2 only! \n")
+        return userChoice()
+
+
+def userPuzzle():
+    print('Please Enter your puzzle with 0 to represent the blank spot \n')
+    row1 = input('Please enter your first row seperating the numbers with a space:\n')
+    row1 = row1.split(' ')
+    row2= input('Please enter your second row:\n')
+    row2 = row2.split(' ')
+    row3 = input('Please enter your third row :\n')
+    row3 = row3.split(' ')
+    puzzle = row1, row2, row3
+    return puzzle
+
+def userStrategy():
+    print('Which strategy would you like to use: \n')
+    print('Uniform Cost Search\n')
+    print('A* with the Misplaced Tile heuristic\n')
+    print('A* with the manhattan distance\n')
+    userStrategyC = input(' ')
+    return userStrategyC
+
+    
 if __name__ == '__main__':
-        main()
+            main()
