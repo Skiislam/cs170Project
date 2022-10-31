ab2f74bf0c4e2529c7b5e9f3bbb3ef347b8503a5
Skiislam <Sisla016@ucr.edu>
2022-10-31 11:06:21 -0700

    Made main + Menu for user

--- CS170P1.py
+++ CS170P1.py
@@ -1,1 +1,17 @@
-#Test to see if gitcommit is working with Vscode
+import queue
+import sys
+
+
+def main():
+    print('Welcome to the 8 Puzzle Solver \n')
+    print('Would you like to use our puzzle, or create your own? \n')
+    userInput = input('Press 1 for the default and 2 if you want to input your own!')
+    choice = int(userInput)
+
+    if choice == 1:
+        puzzle = (['1', '3', '5', ], ['4', '6', '8'], ['2', '7', '8'])
+    elif choice == 2:
+        print('Please begin entering your puzzle and use 0 to represent a blank spot \n')
+        
+if __name__ == '__main__':
+        main()
